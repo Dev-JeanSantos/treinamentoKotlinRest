ec0e26577650ee7feab22b2e75c9eb10588490c0
Dev-JeanSantos <jeancbsan@gmail.com>
2022-09-04 12:27:23 -0300

    creation of the kotlin training project

---
+++ src/main/kotlin/br/com/fourtk/treinamentoKotlin/TreinamentoKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.fourtk.treinamentoKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TreinamentoKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<TreinamentoKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/fourtk/treinamentoKotlin/TreinamentoKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.fourtk.treinamentoKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TreinamentoKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
