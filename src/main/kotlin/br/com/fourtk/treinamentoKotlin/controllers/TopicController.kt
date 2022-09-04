09798887bfe984d695c992d46312ec33ee367baf
Dev-JeanSantos <jeancbsan@gmail.com>
2022-09-04 13:42:05 -0300

    getById endpoint creation in topic

--- src/main/kotlin/br/com/fourtk/treinamentoKotlin/controllers/TopicController.kt
+++ src/main/kotlin/br/com/fourtk/treinamentoKotlin/controllers/TopicController.kt
@@ -1,13 +1,11 @@
 package br.com.fourtk.treinamentoKotlin.controllers
 
-import br.com.fourtk.treinamentoKotlin.model.Course
 import br.com.fourtk.treinamentoKotlin.model.Topic
-import br.com.fourtk.treinamentoKotlin.model.User
 import br.com.fourtk.treinamentoKotlin.services.TopicServices
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.Arrays
 
 @RestController
 @RequestMapping("/topics")
@@ -19,5 +17,10 @@ class TopicController (
     fun list(): List<Topic> {
 
         return topicService.listar()
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): Topic {
+        return topicService.getById(id)
     }
 }
--- src/main/kotlin/br/com/fourtk/treinamentoKotlin/services/TopicServices.kt
+++ src/main/kotlin/br/com/fourtk/treinamentoKotlin/services/TopicServices.kt
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicServices {
+class TopicServices (
+    private var topicos: List<Topic>
+        ){
 
-    fun listar(): List<Topic> {
+    init {
         val topic = Topic(
             id = 1,
             title = "Duvidas Kotlin",
@@ -25,6 +27,46 @@ class TopicServices {
                 email = "jeancbsan@gmail.com"
             )
         )
-        return Arrays.asList(topic, topic,topic)
+        val topic2 = Topic(
+            id = 2,
+            title = "Duvidas Kotlin 2",
+            message = "states no Kotlin",
+            course = Course(
+                id = 1,
+                name = "Kotlin",
+                category = "Programação"
+            ),
+            author = User(
+                id = 3,
+                name = "Marcela Santos",
+                email = "marsan@gmail.com"
+            )
+        )
+        val topic3 = Topic(
+            id = 3,
+            title = "construtores",
+            message = "erro nos construtores do Kotlin",
+            course = Course(
+                id = 1,
+                name = "Kotlin",
+                category = "Programação"
+            ),
+            author = User(
+                id = 2,
+                name = "Ingrid Almeida",
+                email = "almeidaingrid@gmail.com"
+            )
+        )
+        topicos = Arrays.asList(topic, topic2,topic3)
+    }
+
+    fun listar(): List<Topic> {
+        return topicos
+    }
+
+    fun getById(id: Long): Topic {
+        return topicos.stream().filter({
+            t -> t.id == id
+        }).findFirst().get()
     }
 }
